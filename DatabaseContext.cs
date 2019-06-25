64d7a428cddd0c35efd27b8e4964ea672cb73565
KillahBea <bee.noelle6@gmail.com>
2019-06-25 16:49:01 -0400

    updated DatabaseContext

--- DatabaseContext.cs
+++ DatabaseContext.cs
@@ -29,8 +29,8 @@ namespace suncoastoverflow
       if (!optionsBuilder.IsConfigured)
       {
         var envConn = Environment.GetEnvironmentVariable("DATABASE_URL");
-#warning Be sure to update to your correct connection string to the point to the correct database
-        var conn = "server=localhost;database=SdgTemplate";
+#warning 
+        var conn = "server=localhost;database=SdOverflow";
         if (envConn != null)
         {
           conn = ConvertPostConnectionToConnectionString(envConn);
